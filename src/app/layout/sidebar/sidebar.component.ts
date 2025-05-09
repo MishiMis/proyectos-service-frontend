1c6fb22063247b232b090a8c4835a6e3b509fb66
Felix <felyrics3@gmail.com>
2025-05-09 14:50:34 -0500

    refactor: mejorar el diseño y la estructura del componente sidebar
    - Actualizar estilos y clases CSS para un diseño más moderno.
    - Implementar navegación dinámica utilizando un array de elementos de navegación.
    - Añadir un nuevo archivo de configuración para los elementos de navegación.
    - Crear un modelo para los elementos de navegación.

--- src/app/layout/sidebar/sidebar.component.ts
+++ src/app/layout/sidebar/sidebar.component.ts
@@ -1,21 +1,28 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
-
+import { NAV_ITEMS } from './sidebar.config';
+import { NavItem } from './sidebar.model';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-sidebar',
-  imports: [RouterLink, RouterLinkActive],
+  standalone: true,
+  imports: [RouterLink, RouterLinkActive, CommonModule],
   templateUrl: './sidebar.component.html',
-  styleUrl: './sidebar.component.css'
+  styleUrls: ['./sidebar.component.css']
 })
 export class SidebarComponent {
-
+  constructor() {
+    console.log('navItems:', this.navItems);
+  }
+  
   @Input() isCollapsed = false;
   @Output() isCollapsedChange = new EventEmitter<boolean>();
+
+  navItems: NavItem[] = NAV_ITEMS;
 
   toggleCollapse() {
     this.isCollapsed = !this.isCollapsed;
     this.isCollapsedChange.emit(this.isCollapsed);
   }
-
 }
---
+++ src/app/layout/sidebar/sidebar.config.ts
@@ -0,0 +1,22 @@
+import { NavItem } from './sidebar.model';
+
+export const NAV_ITEMS: NavItem[] = [
+  {
+    path: '/dashboard',
+    title: 'Dashboard',
+    icon: 'M4 6a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2H6a2 2 0 01-2-2V6zM14 6a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2V6zM4 16a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2H6a2 2 0 01-2-2v-2zM14 16a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2v-2z',
+    activeClass: 'bg-gray-700 text-white'
+  },
+  {
+    path: '/products',
+    title: 'Productos',
+    icon: 'M20 7l-8-4-8 4m16 0l-8 4m8-4v10l-8 4m0-10L4 7m8 4v10M4 7v10l8 4',
+    activeClass: 'bg-gray-700 text-white'
+  },
+  {
+    path: '/puestos',
+    title: 'Puestos',
+    icon: 'M21 13.255A23.931 23.931 0 0112 15c-3.183 0-6.22-.62-9-1.745M16 6V4a2 2 0 00-2-2h-4a2 2 0 00-2 2v2m4 6h.01M5 20h14a2 2 0 002-2V8a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z',
+    activeClass: 'bg-gray-700 text-white'
+  }
+];
